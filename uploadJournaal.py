2bed8227a4e772923d95caa936cd4443067594dc
onat68 <onat.rigault@gmail.com>
2023-11-08 20:42:02 +0100

    connexion avec la db établie

--- uploadJournaal.py
+++ uploadJournaal.py
@@ -1,19 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import db
 
 cred = credentials.Certificate("journaal-8726c-firebase-adminsdk-7juo8-53044a14de.json")
-app = firebase_admin.initialize_app(cred, name= 'Journaal')
-db = firestore.client()
-entries_ref = db.collection('entries')
-docs = entries_ref.stream()
+app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://journaal-8726c-default-rtdb.europe-west1.firebasedatabase.app/'
+})
 
-for doc in docs:
-    print(doc)
+entries_ref = db.reference('entries')
+print(entries_ref.get())
 
-
-# ref = db.reference('https://journaal-8726c-default-rtdb.europe-west1.firebasedatabase.app')
-# ref = db.child('entries')
-
-# result = firebase_admin.get_app('/entries', None)
-# print(ref)
