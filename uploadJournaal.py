a0f2df439270ddc92ba45343425858e516eed173
onat68 <onat.rigault@gmail.com>
2023-11-08 22:26:36 +0100

    parse et set db ok / remplace tte la db au lieu d'add

--- uploadJournaal.py
+++ uploadJournaal.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import db
 from Entry import Entry
+import json
 import docx2txt
 import re
 
@@ -16,21 +17,16 @@ docEntries.pop(0)
 entries = list()
 for i in range(len(docEntries)):
     splitedEntry = re.split(r'Perso - |Pro -',docEntries[i])
-    entry = Entry(splitedEntry[1],splitedEntry[2],splitedEntry[0])
+    entry = Entry(splitedEntry[1],splitedEntry[2],splitedEntry[0]).dictify()
     entries.append(entry)
-    
-
-
-
-
-
 
 
 cred = credentials.Certificate("journaal-8726c-firebase-adminsdk-7juo8-53044a14de.json")
 app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://journaal-8726c-default-rtdb.europe-west1.firebasedatabase.app/'
 })
-
 entries_ref = db.reference('entries')
-# print(entries_ref.get())
+
+entries_ref.set(entries)
+
 
